c0ab15ce992fca8633d1a572a8503ae52bfd622f
Paul Tagliamonte <tag@pault.ag>
2015-09-18 13:21:11 -0400

    Add in better WMATA

--- cuttlebot.py
+++ cuttlebot.py
@@ -1,9 +1,31 @@
 from butterfield.utils import at_bot
 import asyncio
+import aiowmata.rail
 
 
 @at_bot
 @asyncio.coroutine
-def whosleft(bot, message: "message"):
+def wmata(bot, message: "message"):
+
+    def prediction_to_string(prediction):
+        eta = prediction['Min'].strip()
+        if eta == "BRD":
+            eta = "is boarding now"
+        elif eta == "ARR":
+            eta = "is just arriving"
+        else:
+            eta = "in " + eta + " minutes"
+
+        return ("{Car} car train to {DestinationName} arrriving to "
+                "{LocationName} {eta} (on track {Group})".format(eta=eta, **prediction))
+
     text = message.get('text', '').lower()
-    yield from bot.post(message['channel'], "")
+    if 'wmata' not in text:
+        return
+
+    predictions = yield from aiowmata.rail.get_predictions("B35")
+
+    yield from bot.post(
+        message['channel'],
+        "\n".join((prediction_to_string(x) for x in sorted(predictions['Trains'], key=lambda x: x['Group']))),
+    )
